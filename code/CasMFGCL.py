3cecce36d3cdac3993f560eb7b33c20b9b222c42
YuShengPeng <33218100+ShengPengYu@users.noreply.github.com>
2023-05-25 09:21:27 +0800

    Update CasMFGCL.py

--- code/CasMFGCL.py
+++ code/CasMFGCL.py
@@ -6,11 +6,11 @@ from utils import *
 from dgi import DGI
 
 class Model(nn.Module):
-    def __init__(self, sizes, drug_sim, mic_sim):
+    def __init__(self, sizes, dis_sim, mic_sim):
         super(Model, self).__init__()
         np.random.seed(sizes.seed)
         t.manual_seed(sizes.seed)
-        self.drug_size = sizes.drug_size
+        self.dis_size = sizes.dis_size
         self.mic_size = sizes.mic_size
         self.F1 = sizes.F1
         self.F2 = sizes.F2
@@ -24,27 +24,27 @@ class Model(nn.Module):
         self.lambda2 = sizes.lambda2
 
         self.kernel_len = 4
-        self.drug_ps = t.ones(self.kernel_len) / self.kernel_len
+        self.dis_ps = t.ones(self.kernel_len) / self.kernel_len
         self.mic_ps = t.ones(self.kernel_len) / self.kernel_len
 
-        self.drug_sim = t.DoubleTensor(drug_sim)
+        self.dis_sim = t.DoubleTensor(dis_sim)
         self.mic_sim = t.DoubleTensor(mic_sim)
 
-        self.gcn_1 = conv.GCNConv(self.drug_size + self.mic_size, self.F1)
+        self.gcn_1 = conv.GCNConv(self.dis_size + self.mic_size, self.F1)
         # ---------------------------------------
-        # self.dgi = DGI(self.drug_size + self.mic_size, self.F1, 'prelu')
+        # self.dgi = DGI(self.dis_size + self.mic_size, self.F1, 'prelu')
         # self.b_xent = nn.BCEWithLogitsLoss()
         # ---------------------------------------
         self.gcn_2 = conv.GCNConv(self.F1, self.F2)
         self.gcn_3 = conv.GCNConv(self.F2, self.F3)
 
-        self.alpha1 = t.randn(self.drug_size, self.mic_size).double()
-        self.alpha2 = t.randn(self.mic_size, self.drug_size).double()
+        self.alpha1 = t.randn(self.dis_size, self.mic_size).double()
+        self.alpha2 = t.randn(self.mic_size, self.dis_size).double()
 
-        self.drug_l = []
+        self.dis_l = []
         self.mic_l = []
 
-        self.drug_k = []
+        self.dis_k = []
         self.mic_k = []
 
     def SSL(self, sess_emb_hgnn, sess_emb_lgcn):
@@ -69,14 +69,14 @@ class Model(nn.Module):
         t.manual_seed(self.seed)
         x = input['feature']
         adj = input['Adj']
-        drugs_kernels = []
+        diss_kernels = []
         mic_kernels = []
 
         # -------------------------------------------
-        # idx = np.random.permutation(self.drug_size + self.mic_size)
+        # idx = np.random.permutation(self.dis_size + self.mic_size)
         # shuf_fts = x[idx, :]
-        # lbl_1 = t.ones(1, self.drug_size + self.mic_size)
-        # lbl_2 = t.zeros(1, self.drug_size + self.mic_size)
+        # lbl_1 = t.ones(1, self.dis_size + self.mic_size)
+        # lbl_2 = t.zeros(1, self.dis_size + self.mic_size)
         # lbl = t.cat((lbl_1, lbl_2), 1)
         # logits = self.dgi(x, shuf_fts, adj, False, None, None, None)
         # con_loss = self.b_xent(logits, lbl)
@@ -85,33 +85,33 @@ class Model(nn.Module):
         # -------------------------------------------
 
         H1 = t.relu(self.gcn_1(x, adj['edge_index'], adj['data'][adj['edge_index'][0], adj['edge_index'][1]]))
-        drugs_kernels.append(t.DoubleTensor(getGipKernel(H1[:self.drug_size].clone(), 0, self.h1_gamma, True).double()))
-        mic_kernels.append(t.DoubleTensor(getGipKernel(H1[self.drug_size:].clone(), 0, self.h1_gamma, True).double()))
+        diss_kernels.append(t.DoubleTensor(getGipKernel(H1[:self.dis_size].clone(), 0, self.h1_gamma, True).double()))
+        mic_kernels.append(t.DoubleTensor(getGipKernel(H1[self.dis_size:].clone(), 0, self.h1_gamma, True).double()))
 
         H2 = t.relu(self.gcn_2(H1, adj['edge_index'], adj['data'][adj['edge_index'][0], adj['edge_index'][1]]))
-        drugs_kernels.append(t.DoubleTensor(getGipKernel(H2[:self.drug_size].clone(), 0, self.h2_gamma, True).double()))
-        mic_kernels.append(t.DoubleTensor(getGipKernel(H2[self.drug_size:].clone(), 0, self.h2_gamma, True).double()))
+        diss_kernels.append(t.DoubleTensor(getGipKernel(H2[:self.dis_size].clone(), 0, self.h2_gamma, True).double()))
+        mic_kernels.append(t.DoubleTensor(getGipKernel(H2[self.dis_size:].clone(), 0, self.h2_gamma, True).double()))
 
         H3 = t.relu(self.gcn_3(H2, adj['edge_index'], adj['data'][adj['edge_index'][0], adj['edge_index'][1]]))
-        drugs_kernels.append(t.DoubleTensor(getGipKernel(H3[:self.drug_size].clone(), 0, self.h3_gamma, True).double()))
-        mic_kernels.append(t.DoubleTensor(getGipKernel(H3[self.drug_size:].clone(), 0, self.h3_gamma, True).double()))
+        diss_kernels.append(t.DoubleTensor(getGipKernel(H3[:self.dis_size].clone(), 0, self.h3_gamma, True).double()))
+        mic_kernels.append(t.DoubleTensor(getGipKernel(H3[self.dis_size:].clone(), 0, self.h3_gamma, True).double()))
 
-        drugs_kernels.append(self.drug_sim)
+        diss_kernels.append(self.dis_sim)
         mic_kernels.append(self.mic_sim)
 
-        drug_k = sum([self.drug_ps[i] * drugs_kernels[i] for i in range(len(self.drug_ps))])
-        self.drug_k = normalized_kernel(drug_k)
+        dis_k = sum([self.dis_ps[i] * diss_kernels[i] for i in range(len(self.dis_ps))])
+        self.dis_k = normalized_kernel(dis_k)
         mic_k = sum([self.mic_ps[i] * mic_kernels[i] for i in range(len(self.mic_ps))])
         self.mic_k = normalized_kernel(mic_k)
-        self.drug_l = laplacian(drug_k)
+        self.dis_l = laplacian(dis_k)
         self.mic_l = laplacian(mic_k)
 
         # -------------------------------------------
-        con_loss_1 = self.SSL(drug_k, self.drug_sim)
+        con_loss_1 = self.SSL(dis_k, self.dis_sim)
         con_loss_2 = self.SSL(mic_k, self.mic_sim)
         # -------------------------------------------
 
-        out1 = t.mm(self.drug_k, self.alpha1)
+        out1 = t.mm(self.dis_k, self.alpha1)
         out2 = t.mm(self.mic_k, self.alpha2)
 
         out = (out1 + out2.T) / 2
